a1ad8a1ee1b1268f213fd0fcaf7e30e0be60748f
Denys9 <113623082+Denys9@users.noreply.github.com>
2022-10-08 13:18:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 try:
+    sum = 0
     num = int(input('Введіть число - '))
-    if num >>0 and num !=7:
-        print('Number is positive')
-    elif num << 0 and num !=7:
-        print('Number is negative')
-    elif num == 0:
-        print('Number is equal to zero')
-    elif num ==7:
-        print('Good bye!')
+    while True:
+            num = int(input('Введіть число - '))
+            if num != 7:
+                if num >0:
+                    print('Number is positive')
+                if num < 0:
+                    print('Number is negative')
+                if num == 0:
+                    print('Number is equal to zero')
+            else:
+                print('Good bye!')
 except Exception as ex:
     print(f'Eror information: {ex}')
 finally:
